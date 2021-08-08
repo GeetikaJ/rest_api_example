a9686367696f48da1cb55cc5bd26129020a5ef65
Geetika Joshi <geetika.joshi@akalinfosys.com>
2021-08-09 01:56:17 +0530

    Initial commit

---
+++ api/create_user.php
@@ -0,0 +1,75 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: http://localhost/rest-api-authentication-example/");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// generate json web token
+include_once 'config/core.php';
+include_once 'libs/php-jwt-master/src/BeforeValidException.php';
+include_once 'libs/php-jwt-master/src/ExpiredException.php';
+include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once 'libs/php-jwt-master/src/JWT.php';
+use \Firebase\JWT\JWT;
+ 
+// files needed to connect to database
+include_once 'config/database.php';
+include_once 'objects/user.php';
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// instantiate product object
+$user = new User($db);
+ 
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+ 
+// set product property values
+$user->firstname = $data->firstname;
+$user->lastname = $data->lastname;
+$user->email = $data->email;
+$user->password = $data->password;
+
+$email_exists = $user->emailExists();
+
+if($email_exists){
+	// set response code
+    http_response_code(400);
+ 
+    // display message: unable to create user
+    echo json_encode(array("message" => "Duplicate email ID"));
+} else {
+	// create the user
+if(
+    !empty($user->firstname) &&
+    !empty($user->email) &&
+    !empty($user->password) &&
+    $user->create()
+){
+ 
+    // set response code
+    http_response_code(200);
+
+
+    // display message: user was created
+    echo json_encode(array("message" => "User was created."));
+}
+ 
+// message if unable to create user
+else{
+ 
+    // set response code
+    http_response_code(400);
+ 
+    // display message: unable to create user
+    echo json_encode(array("message" => "Unable to create user."));
+}
+	
+}
+ 
+
+?>
